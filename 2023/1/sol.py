6a1f4096f2037f05b4105de1104d1bf82e0f0c40
Erick Hammersmark <bink@uffda.com>
2023-11-30 23:28:27 -0800

    day 1 part 2

--- 2023/1/sol.py
+++ 2023/1/sol.py
@@ -12,5 +12,39 @@ with open("input.txt", "r") as INPUT:
     for line in INPUT.readlines():
         digits = list(filter(lambda x: x >= "0" and x <= "9", line))
         sum += int(digits[0] + digits[-1])
-print(sum)
+print(f"Part 1: {sum}")
 
+
+def build_trie(*args):
+    trie = {}
+    for idx, arg in enumerate(args):
+        head = trie
+        for letter in arg:
+            head[letter] = head.get(letter, {})
+            head = head[letter]
+        head["value"] = str(idx)
+    return trie
+
+def try_trie(trie, letters):
+    head = trie
+    for letter in letters:
+        if "value" in head:
+            return head["value"]
+        if letter not in head:
+            return None
+        head = head[letter]
+
+trie = build_trie("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+for digit in range(0, 10):
+    trie[str(digit)] = {"value": str(digit)}
+
+sum = 0
+with open("input.txt", "r") as INPUT:
+    for line in INPUT.readlines():
+        digits = []
+        for idx, char in enumerate(line):
+            value = try_trie(trie, line[idx:])
+            if value is not None:
+                digits.append(value)
+        sum += int(digits[0] + digits[-1])
+print(f"Part 2: {sum}")
