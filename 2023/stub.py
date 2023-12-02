c7700f51fc84c4be23d0dbce3fbd7bd4de670fe4
Erick Hammersmark <bink@uffda.com>
2023-12-02 10:02:32 -0800

    common functions and starting point

---
+++ 2023/stub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append("..")
+
+from argparse import ArgumentParser
+from lib import *
+
+def parse_args():
+    parser = ArgumentParser()
+    return parser.parse_args()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
