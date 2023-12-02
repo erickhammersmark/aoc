f62b96cc577f216998895a399b8204f3b2ba3a7c
Erick Hammersmark <bink@uffda.com>
2023-12-02 10:03:05 -0800

    day 2

---
+++ 2023/2/sol.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append("..")
+
+from argparse import ArgumentParser
+from lib import *
+
+def parse_args():
+    parser = ArgumentParser()
+    return parser.parse_args()
+
+def main():
+    limits = { "red": 12, "green": 13, "blue": 14 }
+    _sum = 0
+    powersum = 0
+    
+    for line in input():
+        atleast = {"red": 0, "green": 0, "blue": 0}
+        game, blocks = line.split(":")
+        game_no = int(game.split()[1])
+
+        draws = blocks.split(";")
+        for draw in draws:
+            blockcounts = draw.split(",")
+            for blockcount in blockcounts:
+                count, color = blockcount.split()
+                count = int(count)
+                if count > atleast[color]:
+                    atleast[color] = count
+                if count > limits[color]:
+                    #print(f"Game {game_no} impossible as {color} count of {count} exceeds limit of {limits[color]}")
+                    game_no = 0
+        _sum += game_no
+        powersum += atleast["red"] * atleast["green"] * atleast["blue"]
+    print(_sum)
+    print(powersum)
+
+if __name__ == "__main__":
+    main()
+
+"""
+['Game', '1:', '5', 'red,', '1', 'green,', '2', 'blue;', '2', 'green,', '8', 'blue,', '6', 'red;', '8', 'red,', '3', 'blue,', '2', 'green;', '6', 'red,', '1', 'green,', '19', 'blue;', '1', 'red,', '17', 'blue']
+['Game', '2:', '4', 'red,', '5', 'green,', '2', 'blue;', '7', 'red,', '14', 'green,', '3', 'blue;', '2', 'green,', '5', 'blue,', '11', 'red;', '10', 'blue,', '3', 'green;', '9', 'green,', '6', 'blue,', '13', 'red;', '7', 'red,', '5', 'green,', '9', 'blue']
+['Game', '3:', '9', 'green,', '18', 'blue,', '1', 'red;', '6', 'red,', '10', 'blue,', '5', 'green;', '4', 'blue,', '4', 'red,', '15', 'green']
+['Game', '4:', '1', 'red,', '13', 'green;', '10', 'green,', '2', 'red;', '3', 'red,', '4', 'green,', '2', 'blue']
+['Game', '5:', '4', 'red,', '2', 'green,', '1', 'blue;', '4', 'red,', '9', 'blue;', '4', 'green,', '1', 'red,', '6', 'blue;', '3', 'blue,', '2', 'green,', '6', 'red;', '5', 'red,', '4', 'green,', '1', 'blue']
+"""
