998d3da2ed4b12b8092f32c17f0339f08cb93e24
Erick Hammersmark <bink@uffda.com>
2023-12-08 11:17:59 -0800

    day 8

---
+++ 2023/8/sol.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import math
+import sys
+
+from argparse import ArgumentParser
+
+sys.path.append("..")
+from lib import read_input
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--filename", default="input.txt")
+    parser.add_argument("--two", action="store_true", default=False)
+    return parser.parse_args()
+
+args = parse_args()
+
+lines = read_input(filename=args.filename)
+
+ops = lines[0]
+
+tree = {}
+for line in lines[2:]:
+    tree[line[0:3]] = [line[7:10], line[12:15]]
+
+def traverse(node, end):
+    count = 0
+    while True:
+        for op in (0 if o == "L" else 1 for o in ops):
+            count += 1
+            node = tree[node][op]
+            if node.endswith(end):
+                return count
+
+if args.two:
+    counts = [traverse(node, "Z") for node in tree.keys() if node.endswith("A")]
+    print(math.lcm(*counts))
+else:
+    print(traverse("AAA", "ZZZ"))
