8d34f632b32be0912801aabcdd85d4249cc5007e
Erick Hammersmark <bink@uffda.com>
2024-12-02 08:10:54 -0800

    bring in stub and lib from 2023

---
+++ 2024/lib.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+def read_input(filename="input.txt"):
+    with open(filename, "r") as INPUT:
+        return [l.rstrip("\n") for l in INPUT.readlines()]
+
+
+def build_trie(*args, values=None):
+    trie = {}
+    for idx, arg in enumerate(args):
+        head = trie
+        for letter in arg:
+            head[letter] = head.get(letter, {})
+            head = head[letter]
+        if values is not None:
+            head["value"] = values[idx]
+        else:
+            head["value"] = str(idx)
+    return trie
+
+def try_trie(trie, letters):
+    head = trie
+    for letter in letters:
+        if "value" in head:
+            return head["value"]
+        if letter not in head:
+            return None
+        head = head[letter]
+
---
+++ 2024/stub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append("..")
+
+from argparse import ArgumentParser
+from lib import *
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--filename", default="input.txt")
+    parser.add_argument("--two", action="store_true", default=False)
+    return parser.parse_args()
+
+args = parse_args()
+
+
