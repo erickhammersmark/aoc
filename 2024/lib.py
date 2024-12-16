12cfccebfc34eb51ac72e2f129875d402f602ca4
Erick Hammersmark <bink@uffda.com>
2024-12-16 11:32:40 -0800

    lib.py

--- 2024/lib.py
+++ 2024/lib.py
@@ -5,13 +5,38 @@ def read_input(filename="input.txt"):
         return [l.rstrip("\n") for l in INPUT.readlines()]
 
 class Board(list):
-    def __init__(self, filename=None):
+    def __init__(self, filename=None, try_type=None):
         if filename is not None:
             lines = read_input(filename=filename)
         else:
             lines = read_input()
         for line in lines:
-            self.append(list(line))
+            line = list(line)
+            if try_type is not None:
+                for idx, val in enumerate(line):
+                    try:
+                        line[idx] = try_type(val)
+                    except:
+                        pass
+            self.append(line)
+
+    def get(self, pos, default=None):
+        try:
+            return self[pos[0]][pos[1]]
+        except Exception as e:
+            return default
+
+    def neighbors(self, pos, diagonal=False):
+        neighbors = []
+        for row in range(pos[0] - 1, pos[0] + 2):
+            for col in range(pos[1] - 1, pos[1] + 2):
+                if row == pos[0] and col == pos[1]:
+                    continue
+                if not self.oob((row, col)):
+                    if not diagonal and row != pos[0] and col != pos[1]:
+                        continue
+                    neighbors.append((row, col))
+        return neighbors
 
     def copy(self):
         """
